d4222c455a6cf802dc907c9bad9a8969b6d64834
Cem Gencer <cemgencer.1988@gmail.com>
2022-11-19 13:24:42 +0300

    asal2

--- src/asal_sayi.java
+++ src/asal_sayi.java
@@ -6,8 +6,16 @@ public class asal_sayi {
         Scanner scan =new Scanner(System.in);
         System.out.print("Bir Sayi Giriniz :");
         number= scan.nextInt();
-        for (int i=2;i<number;i++){
-            if (number % i==0){
+        if (number < 1){
+            System.out.println("Geçersiz Bir Sayı");
+            return;
+        }
+        if (number == 1){
+            System.out.println("Sayı Asal Bir Sayı Değildir .");
+            return;
+        }
+        for (int i = 2 ;i < number ; i++){
+            if (number % i == 0){
                 asal=false;
             }
         }
